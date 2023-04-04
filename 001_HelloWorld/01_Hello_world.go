e148da57d98036436a3e0a1ba428b4f2f4e31b4c
Mathes Henrique dos Santos Lima <1995.matheus.lima@gmail.com>
2023-04-04 12:49:48 -0300

    baby steps on goLang, Learned how to print 'hello World'

---
+++ 001_HelloWorld/01_Hello_world.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World")
+	foo()
+	for i := 0; i < 11; i++ {
+		if i%2 == 0 {
+			fmt.Println(i)
+		}
+
+	}
+}
+
+func foo() {
+	fmt.Println("I'm  in foo")
+}
